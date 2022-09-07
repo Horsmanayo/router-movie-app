1a56b4e823b83ba8a9d2ff05bfdf95c59f4874e0
Horsmanayo <oniademola001@gmail.com>
2022-09-07 23:14:04 +0100

    add to fav fix

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,24 +1,25 @@
-import React, { useEffect } from "react";
+import React from "react";
 
+const MovieList = ({ movies, handleFavoriteClicks, favoriteComponent }) => {
+  const FavoriteComponent = favoriteComponent;
 
-const MovieList = (props) => {
-    const FavoriteComponent = props.favoriteComponent 
+  return (
+    <>
+      {movies?.map((movie, key) => (
+        <div className="col" key={movie?.imdbID}>
+          <div className="image-container d-flex jusitfy-content-start m-3">
+            <img src={movie?.Poster} alt="movie" />
+            <div
+              onClick={() => handleFavoriteClicks(movie)}
+              className="overlay d-flex align-items-center justify-context-center"
+            >
+              <FavoriteComponent />
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
 
-   
-    
-    return (
-        <>
-            {props.movies.map((movie, key) => <div className="col">
-                <div className="image-container d-flex jusitfy-content-start m-3">
-                    <img src={movie.Poster} alt= "movie"/>  
-                    <div onClick={()=>props.handleFavoriteClicks(movie )} className="overlay d-flex align-items-center justify-context-center">
-                        <FavoriteComponent />
-                    </div>
-                </div>
-                 
-            </div>)}
-        </>  
-    )
-}
-
-export default  MovieList
+export default MovieList;
